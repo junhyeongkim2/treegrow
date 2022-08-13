2656341045768bfa6230424100a0e6ed298b0625
jun <ggprgrkjh@naver.com>
2022-08-14 04:53:27 +0900

    feat: users and commits added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { CommitsController } from './commits/commits.controller';
+import { CommitsService } from './commits/commits.service';
+import { CommitsModule } from './commits/commits.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, CommitsModule],
+  controllers: [AppController, UsersController, CommitsController],
+  providers: [AppService, UsersService, CommitsService],
 })
 export class AppModule {}
