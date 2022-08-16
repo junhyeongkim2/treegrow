aa02ddd457a07e87400a93d20b9d94631479fdd6
jun <ggprgrkjh@naver.com>
2022-08-17 05:30:18 +0900

    feat: docker-compose updated

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: '172.18.0.2',
+        host: 'mysql',
         port: 3306,
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
+        username: 'junhyeong',
+        password: 'password',
+        database: 'treegrow',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
